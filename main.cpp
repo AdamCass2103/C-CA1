cb37cb4ccede887ad5b9090e43dd57d886aac67a
Adam Cassidy <D00265095@student.dkit.ie>
2025-02-25 13:02:16 +0000

    CA1

--- main.cpp
+++ main.cpp
@@ -13,5 +13,45 @@ struct Movie {
     string title;
     string genre;
     int year;
-    float revenue;
+    int revenue;
 };
+
+
+void loadCSV(vector<Movie> &movies, const string &filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error opening file!" << endl;
+        return;
+    }
+
+    string line;
+    getline(file, line);  // Skip header row
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Movie movie;
+        string field;
+
+        // Read ID
+        getline(ss, field, ',');
+        movie.id = stoi(field);
+
+        // Read Title
+        getline(ss, movie.title, ',');
+
+        // Read Genre
+        getline(ss, movie.genre, ',');
+
+        // Read Year
+        getline(ss, field, ',');
+        movie.year = stoi(field);
+
+        // Read Revenue
+        getline(ss, field, ',');
+        movie.revenue = stof(field);
+
+        // Add to vector
+        movies.push_back(movie);
+    }
+
+    file.close();
+}
